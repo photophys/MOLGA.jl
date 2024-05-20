6e64c25de457b842f2f3e83c0f0c2f88874762d6
migatt <migatt@a1.net>
2024-05-20 20:59:43 +0200

    Implement export_xyz function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,27 @@
-using MOLGA
+using MOLGA, MOLGA.Types
 using Test, Documenter
 
 @testset verbose = true "MOLGA.jl" begin
     @testset "IO" begin
         @test MOLGA.IO.load_xyz("samples/co2.xyz") == [
-            MOLGA.Types.BaseAtom(6, [-1.1619373, 0.0, -1.704638554]),
-            MOLGA.Types.BaseAtom(8, [-0.610097929, -0.126832008, -0.681728275]),
-            MOLGA.Types.BaseAtom(8, [-1.713776672, 0.126832008, -2.727548833]),
+            BaseAtom(6, [-1.1619373, 0.0, -1.704638554]),
+            BaseAtom(8, [-0.610097929, -0.126832008, -0.681728275]),
+            BaseAtom(8, [-1.713776672, 0.126832008, -2.727548833]),
         ]
-    end
 
-    @testset "GA-Utils" begin
-        pos = MOLGA.Types.Vec([1.282131000, 0.737863000, -0.041933000])
-        atoms = [MOLGA.Types.BaseAtom(6, [1.619507883, -0.147537000, -0.210363518])]
-
-        # distance between both atoms is 0.96235 (Chemcraft)
-        test_check_distance(min::Float64, max::Float64) =
-            MOLGA.GeneticAlgorithm.Utils.check_distance(
-                pos, atoms, MOLGA.Configuration.DistanceThresholds(min, max)
-            )
-
-        @test test_check_distance(0.5, 5.0) == true
-        @test test_check_distance(1.0, 5.0) == false
-        @test test_check_distance(0.5, 0.75) == false
+        atoms = [
+            Atom(8, [-1.674872668, 0.000000000, -0.984966492], 1, 1),
+            Atom(1, [-1.674872668, 0.759337000, -0.388923492], 2, 2),
+            Atom(1, [-1.674872668, -0.759337000, -0.388923492], 3, 2),
+        ]
+        MOLGA.IO.export_xyz(
+            [
+                Structure(; atoms, energy=56.13537, age=10),
+                Structure(; atoms, nuclear_repulsion_energy=1.006),
+            ],
+            "temp-h2o.xyz",
+        )
+        @test success(`diff temp-h2o.xyz 'samples/h2o-structure-example.xyz'`)
     end
 
     doctest(MOLGA)
