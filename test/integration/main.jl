76ed839ae3f603c22b25793c5a375bfd44f2a661
migatt <migatt@a1.net>
2024-05-23 21:41:06 +0200

    Use seeded rng for regression test

--- test/integration/main.jl
+++ test/integration/main.jl
@@ -1,6 +1,6 @@
 using Random
 
 @testset verbose = true "Main" begin
-    run_genetic_algorithm("integration/config/example.toml")
+    run_genetic_algorithm("integration/config/example.toml"; rng=Xoshiro(123))
     @test success(`diff results.xyz integration/xyz/main.xyz`)
 end
